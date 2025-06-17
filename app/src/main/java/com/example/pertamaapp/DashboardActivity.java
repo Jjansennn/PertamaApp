1055eaed0083456700d500138327b9cc98100098
Jjansennn <jansenjansenkhang@gmail.com>
2025-06-17 11:17:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertamaapp/DashboardActivity.java
@@ -0,0 +1,53 @@
+package com.example.pertamaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DashboardActivity extends AppCompatActivity {
+    LinearLayout llyProfil,llyTodolist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        llyProfil= findViewById(R.id.llyProfil);
+        llyTodolist=findViewById(R.id.llyTodolist);
+        llyProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toProfil();
+            }
+        });
+        llyTodolist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toTodolist();
+            }
+        });
+    }
+    public void toProfil(){
+        Intent intent = new Intent(this,ProfilActivity.class);
+        intent.putExtra("nama","Satria");
+        startActivity(intent);
+    }
+
+    public void toTodolist(){
+        Intent intent = new Intent(this,TodoListActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/pertamaapp/ProfilActivity.java
@@ -0,0 +1,139 @@
+package com.example.pertamaapp;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfilActivity extends AppCompatActivity {
+    Button btnSubmit;
+    EditText edtNama, edtEmail;
+    TextView txvHasil;
+    Spinner sprProdi;
+    RadioButton rdbPria, rdbWanita;
+    RadioGroup rdgJenisKelamin;
+    CheckBox ckbMakan,ckbTidur, ckbBelajar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profil);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        init();
+        edtNama.setText(getIntent().getStringExtra("nama"));
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                simpan();
+            }
+        });
+    }
+    public void init(){
+        btnSubmit = findViewById(R.id.btnSubmit);
+        edtNama = findViewById(R.id.edtNama);
+        edtEmail = findViewById(R.id.edtEmail);
+        txvHasil = findViewById(R.id.txvHasil);
+        sprProdi = findViewById(R.id.sprProdi);
+        rdgJenisKelamin = findViewById(R.id.rdgJenisKelamin);
+        rdbPria = findViewById(R.id.rdbPria);
+        rdbWanita = findViewById(R.id.rdbWanita);
+        ckbTidur = findViewById(R.id.ckbTidur);
+        ckbMakan = findViewById(R.id.ckbMakan);
+        ckbBelajar = findViewById(R.id.ckbBelajar);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.prodi_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        sprProdi.setAdapter(adapter);
+    }
+    public boolean isValidated(){
+        if(edtNama.getText().toString().equals("")){
+            edtNama.setError("Nama wajib di isi");
+            return false;
+        }else if(edtEmail.getText().toString().equals("")) {
+            edtEmail.setError("Email wajib di isi");
+            return false;
+        }
+        return true;
+    }
+    public void simpan(){
+        if(isValidated()){
+            // Menampilkan hasil di txvHasil
+            String nama = edtNama.getText().toString();
+            String email = edtEmail.getText().toString();
+            String prodi = sprProdi.getSelectedItem().toString();
+            String jenisKelamin = "";
+            String hobi="";
+            if(rdbWanita.isChecked()) jenisKelamin= rdbWanita.getText().toString();
+            else if(rdbPria.isChecked()) jenisKelamin= rdbPria.getText().toString();
+
+            if(ckbBelajar.isChecked()) hobi += ckbBelajar.getText().toString()+"; ";
+            if(ckbMakan.isChecked()) hobi += ckbMakan.getText().toString()+"; ";
+            if(ckbTidur.isChecked()) hobi += ckbTidur.getText().toString()+"; ";
+
+            txvHasil.setText(nama + "("+jenisKelamin+")"+
+                    "\nHobi "+hobi
+                    + "\n"+ email + "\nProgram Studi " + prodi + "\n" +
+                    getNamaFakultas(prodi));
+        }
+    }
+
+    String getNamaFakultas(String prodi){
+        String namaProdi = prodi.toLowerCase();
+        if(namaProdi.equals("sistem informasi") || namaProdi.equals("informatika")){
+            return "Fakultas Teknologi Informasi";
+        }
+        else if(namaProdi.equals("hukum") || namaProdi.equals("law")){
+            return "Fakultas Hukum";
+        }
+        else if(namaProdi.equals("akuntansi") || namaProdi.equals("manajemen")||
+                namaProdi.equals("perhotelan")){
+            return "Fakultas Ekonomi dan Bisnis";
+        }
+        else
+            return "Fakultas Tidak di Temukan";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_profil, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+        if(item.getItemId()==R.id.mSimpan){
+            simpan();
+            return true;
+        }
+        else{
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
